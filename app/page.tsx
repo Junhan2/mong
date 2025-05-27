76155250c9e5da58a968968dc0f32b78994b5998
gangjoon <102278409+Junhan2@users.noreply.github.com>
2025-05-27 15:29:15 +0900

    debug: Temporarily disable ParticleBackground to isolate React Hook #310 error
    - Remove ParticleBackground component to test if error persists
    - Keep simple black background for now
    - Helps identify if the error comes from MongTodo or other components
    - Will re-enable once core issue is resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
 import MongTodo from "@/components/MongTodo"
-import ParticleBackground from "@/components/ParticleBackground"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-4 bg-black">
-      <ParticleBackground 
-        count={100}
-        spread={10}
-        speed={0.1}
-        baseSize={50}
-        mouseInteraction={true}
-      />
       <MongTodo />
     </main>
   )
