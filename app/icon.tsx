f99b98111c83c0ca8058eb8467427c74ec19c8c4
gangjoon <102278409+Junhan2@users.noreply.github.com>
2025-05-27 10:42:36 +0900

    Remove problematic icon component

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,27 +1,3 @@
-import { ImageResponse } from 'next/og'
-
-export function GET() {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          fontSize: 32,
-          background: 'black',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          color: 'yellow',
-          fontWeight: 'bold',
-        }}
-      >
-        T
-      </div>
-    ),
-    {
-      width: 32,
-      height: 32,
-    }
-  )
+export default function Icon() {
+  return null
 }
